a317b0225a55b14242d5b6ad4d64afbaf3d4d669
Дмитрий Городенцев <dima9720058@live.com>
2018-03-02 21:55:13 +0700

    all bugs has been fixed(mb)

--- l1/l1/main.cpp
+++ l1/l1/main.cpp
@@ -7,17 +7,19 @@ const size_t col = 4;
 
 using namespace std;
 
-void show_matrix(vector<vector<float> > &a, size_t row, size_t col)
+void show_matrix(vector<vector<float> > &a)
 {
+	cout.precision(2);
 	cout << endl << endl;
 	for (size_t i = 0; i < row; i++) {
 		for (size_t j = 0; j < col; j++)
-			cout << setw(6) << a[i][j] << " ";
+			cout << setw(7) << a[i][j] << " ";
 		cout << endl;
 	}
 	cout << endl << endl;
 }
-void Gauss_method(vector<vector<float> > &a, size_t row, size_t col) {
+void Gauss_method(vector<vector<float> > &a) 
+{
 	float mpl = 0;//множитель
 	//прямой ход
 	cout << endl << "PR HOD:" << endl;
@@ -30,7 +32,7 @@ void Gauss_method(vector<vector<float> > &a, size_t row, size_t col) {
 				a[i][j] = a[i][j] - (a[n][j] * mpl);
 		}
 	}
-	show_matrix(a, row, col);
+	show_matrix(a);
 	//обратный ход
 	cout << endl << "OBR HOD: " << endl;
 	for (int n = row - 1; n >= 0; n--) {
@@ -40,36 +42,44 @@ void Gauss_method(vector<vector<float> > &a, size_t row, size_t col) {
 			a[i][col - 1] -= a[n][col - 1] * mpl;
 		}
 	}
-	show_matrix(a, row, col);
+	show_matrix(a);
+}
+
+void up_main_el(vector<vector<float> > &a)
+{
+	for (size_t i = 1; i < row; i++)
+		if (a[0][0] < a[i][0])
+			for (size_t j = 0; j < col; j++)
+				swap(a[i][j], a[0][j]);
+}
+
+void find_answer(float * x, vector<vector<float> > &a)
+{
+	for (int i = 0; i < row; i++)
+		x[i] = a[i][col - 1];
 }
 
 int main()
 {
 	float x[row - 1];
-	vector<vector<float>> v = { { -1, 4, 2, 7 },
-	{ 2, -6, -3, 10 },
-	{ 2, -5, -2, -7}, };
-	cout << "Matrix:" << endl;
-	show_matrix(v, row, col);
-	//поднимаем строку с самым маленьким первым элементом
-	for (size_t i = 1; i < row; i++)
-		if (v[0][0] < v[i][0])//если верхний элемент меньше, то свап на больший элемент
-			for (size_t j = 0; j < col; j++)
-				swap(v[i][j], v[0][j]);
-	cout << endl << "After SWAP ROWS: " << endl;
-	show_matrix(v, row, col);
+	vector<vector<float>> v = { { -1, -1, 1, -1 },{ 2, -1 , 2 ,3 },{ 3, -1, 1, 3 } };
 	
-	Gauss_method(v, row, col);
+	cout << "Matrix:" << endl;
+	show_matrix(v);
+	
+	up_main_el(v);
+	cout << endl << "After SWAP ROWS: " << endl;
+	show_matrix(v);
+	
+	Gauss_method(v);
 	cout << endl << "After Gauss method: " << endl;
-	show_matrix(v, row, col);
+	show_matrix(v);
 
-	//находим корни в матрице
-	for (int i = 0; i < row; i++)
-		x[i] = v[i][col - 1];
-	//Вывод результата
+	find_answer(x, v);
 	cout << "Answer: ";
 	for (int i = 0; i < row; i++)
 		cout << x[i] << " ";
 	cout << endl;
+
 	return 0;
 }
